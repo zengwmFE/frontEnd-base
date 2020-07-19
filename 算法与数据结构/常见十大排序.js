982874f7026a5f0124c3e85c05cf119c21992e15
卫明 <983423202@qq.com>
2020-07-19 23:51:20 +0800

    feat:10大排序-堆排序

--- 算法与数据结构/常见十大排序.js
+++ 算法与数据结构/常见十大排序.js
@@ -295,26 +295,38 @@ console.log(quickSort(arr2))
  */
 
  // 交换两个节点
-var len;    // 因为声明的多个函数都需要数据长度，所以把len设置成为全局变量
-const swap = (array, i, j) => {
-	let temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
-};
+ function heapify(arr, i) {     // 堆调整
+    var left = 2 * i + 1,
+        right = 2 * i + 2,
+        largest = i;
+
+    if (left < len && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < len && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr, i, largest);
+        heapify(arr, largest);
+    }
+}
+
+function swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
 function heapSort(arr) {
     buildMaxHeap(arr);
 
-    
+    for (var i = arr.length-1; i > 0; i--) {
+        swap(arr, 0, i);
+        len--;
+        heapify(arr, 0);
+    }
     return arr;
-}
-function buildMaxHeap(arr){
-      // 初始化大顶堆，从第一个非叶子结点开始
-    len = arr.length
-	for (let i = Math.floor(array.length / 2 - 1); i >= 0; i--) {
-		heapify(array, i);
-	}
-}
-function heapify(arr,i){
-
 }
